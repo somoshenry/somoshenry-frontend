44d4c001f7cbe74fa1dc005f4af1f6e6dd1cc896
RYFP <rotceh.figueroa98@gmail.com>
2025-11-02 22:09:36 -0600

    Agregue botones al sideBar y una estatica

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@/components/login/register/RegisterForm";
 
 export const Register = () => {
   return (
-    <div className="flex min-h-screen items-start justify-center bg-neutral-50 dark:bg-[#121212] py-20">
+    <div className="flex min-h-screen items-start justify-center bg-neutral-50 dark:bg-gray-900 py-20">
       <div className="flex w-full flex-col items-center justify-center">
         <RegisterForm />
       </div>
